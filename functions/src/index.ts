3f52ffeed8d6fcaf0b8044f31fb8fea040146075
Tom Wu <tom.wu@genie-networks.com>
2018-10-24 01:59:38 +0800

    hello world graphql

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,4 +1,5 @@
 import * as functions from 'firebase-functions';
+import { configureServer } from './server';
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
@@ -6,3 +7,8 @@ import * as functions from 'firebase-functions';
 // export const helloWorld = functions.https.onRequest((request, response) => {
 //  response.send("Hello from Firebase!");
 // });
+
+//initialize the server
+const server = configureServer();
+// create and export the api
+export const api = functions.https.onRequest(server);
---
+++ functions/src/server.ts
@@ -0,0 +1,36 @@
+// require all dependencies to set up server
+import * as express from "express";
+import { ApolloServer, gql } from "apollo-server-express";
+
+// cors allows our server to accept requests from different origins
+import * as cors from "cors";
+
+export function configureServer() {
+  // invoke express to create our server
+  const app = express();
+  //use the cors middleware
+  app.use(cors());
+
+  // Simple graphql schema
+  const typeDefs = gql`
+    type Query {
+      "A simple type for getting started!"
+      hello: String
+    }
+  `;
+  // Very simple resolver that returns "world" for the hello query
+  const resolvers = {
+    Query: {
+      hello: () => "world"
+    }
+  };
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers
+  });
+  // now we take our newly instantiated ApolloServer and apply the
+  // previously configured express application
+  server.applyMiddleware({ app });
+  // finally return the application
+  return app;
+}
